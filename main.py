73a4191e27dde23dda39177626e08e5d7e3fb543
TimeElysium <67021200+TimeElysium@users.noreply.github.com>
2021-11-01 16:04:08 +0100

    Exercise 2.3

--- main.py
+++ main.py
@@ -33,7 +33,13 @@ def completeAnalysis(inpath):
     # Part 4 :: Verteilung der Satzlänge graphisch darstellen (x: Satzlänge, y: Wie häufig)?
     y = []
 
-    # Part 5 :: Welche Links
+    # Part 5 :: Welche Links (QSLinks, OLinks) werden von welchen Präpositionen (markiert durch SPATIAL_SIGNAL) getriggert (z.B. wie oft werden QSLinks durch die Präposition „on“ getriggert)?
+    qsLinks = []
+    oLinks = []
+    res5 = [{},{}]
+
+    # Part 6 :: Welches sind die fünf häufigsten "MOTION" Verben (und wie oft kommen diese vor)?
+    res6 = {}
 
     for f in allFiles:
         tree = ET.parse(f)
@@ -59,6 +65,20 @@ def completeAnalysis(inpath):
                 if len(y) - 1 < sLen:
                     y += [0 for k in range(len(y) - 1,sLen)]
                 y[sLen] += 1
+        # Part 5
+        for c in root.findall("TAGS/SPATIAL_SIGNAL"):
+            for cqs in root.findall("TAGS/QSLINK"):
+                if cqs.get("trigger") == c.get("id"):
+                    qsLinks.append(c.get("text"))
+            for co in root.findall("TAGS/OLINK"):
+                if co.get("trigger") == c.get("id"):
+                    oLinks.append(c.get("text"))
+        # Part 6
+        for c in root.findall("TAGS/MOTION"):
+            if c.get("text") not in res6:
+                res6[c.get("text")] = 0
+            res6[c.get("text")] += 1
+        
     
     # Part 1
     print("Wie oft kommen welche PoS-Tags vor?")
@@ -85,8 +105,33 @@ def completeAnalysis(inpath):
     plt.xlabel("Satzlänge")
     plt.ylabel("Häufigkeit")
     plt.bar(x[1:],y[1:])
-    plt.show()
 
+    # Part 5
+    for w in qsLinks:
+        if w not in res5[0]:
+            res5[0][w] = 0
+        res5[0][w] += 1
+    for w in oLinks:
+        if w not in res5[1]:
+            res5[1][w] = 0
+        res5[1][w] += 1
+    print("QsLinks werden getriggert durch: ")
+    for k in res5[0]:
+        print(str(k) + ": " + str(res5[0][k]))
+    print()
+    print("OLinks werden getriggert durch: ")
+    for k in res5[1]:
+        print(str(k) + ": " + str(res5[1][k]))
+    print()
+
+    # Part 6
+    print("Die fünf häufigsten MOTION-Verben sind:")
+    for k in sorted(res6, key=res6.get, reverse=True)[:5]:
+        print(str(k) + ": " + str(res6[k]))
+
+
+
+    plt.show()
 
 
 
