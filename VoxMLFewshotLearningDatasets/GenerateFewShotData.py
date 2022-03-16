6afae11070f755600e650bacd06a2fb7afd5379b
Lukas Geis <lukasgeis@gmx.de>
2022-03-16 13:46:29 +0100

    better fewshot

--- VoxMLFewshotLearningDatasets/GenerateFewShotData.py
+++ VoxMLFewshotLearningDatasets/GenerateFewShotData.py
@@ -1,4 +1,3 @@
-# Imports
 import os
 import pickle
 import random
@@ -6,7 +5,7 @@ import argparse
 import numpy as np
 import xml.etree.ElementTree as ET
 
-def classifyObjects(inpath: str, attribute: str) -> dict:
+def classifyObjects(inpath: str, attribute: str, labelList: list) -> dict:
     if not os.path.exists(inpath):
         return {}
     
@@ -16,43 +15,43 @@ def classifyObjects(inpath: str, attribute: str) -> dict:
             val = ET.parse(os.path.join(inpath, file)).getroot().findtext(attribute)
             if val not in result:
                 result[val] = []
-            result[val].append(file[:-4])
+            result[val].append(labelList.index(file[:-4]))
         except Exception:
             print("ERROR while parsing " + str(file))
 
     return result
 
-def generateFewshotData(way: int, shot: int, ind: int, labelDict: dict, inpath: str, outpath: str) -> None:
+def generateFewshotData(way: int, shot: int, ind: int, labelDict: dict, train: str, test: str, outpath: str) -> None:
+    with open(train, "rb") as f:
+        trainPoints, trainLabels = pickle.load(f)
+    with open(test, "rb") as f:
+        testPoints, testLabels = pickle.load(f)
+
     trainDataset = []
     testDataset = []
+    trainDict = {}
+    testDict = {}
 
-    objects = os.listdir(inpath)
-
+    for point, label in zip(trainPoints, trainLabels):
+        label = label[0]
+        if label not in trainDict:
+            trainDict[label] = []
+        trainDict[label].append(point)
+    for point, label in zip(testPoints, testLabels):
+        label = label[0]
+        if label not in testDict:
+            testDict[label] = []
+        testDict[label].append(point)
+    
     for i in range(way):
         key = random.choice(list(labelDict.keys()))
         for j in range(shot):
-            trainObj = None
-            while trainObj not in objects:
-                trainObj = random.choice(labelDict[key])
-            trainObjPath = os.path.join(inpath, trainObj, "train")
-            trainObjData = [[float(y) for y in x.replace("\n", "").split(" ") if y != ""] for x in open(os.path.join(trainObjPath, random.choice(os.listdir(trainObjPath))), "r").readlines()[1:]]
-            trainData = np.ndarray(shape = (len(trainObjData), 3), dtype = float)
-            for k in range(len(trainObjData)):
-                for l in range(min(3,len(trainObjData[i]))):
-                    trainData[k][l] = trainObjData[k][l]
-            trainDataset.append((trainData, i, key))
+            data = random.choice(trainDict[random.choice(labelDict[key])])
+            trainDataset.append((data, i, key))
         for j in range(20):
-            testObj = None
-            while testObj not in objects:
-                testObj = random.choice(labelDict[key])
-            testObjPath = os.path.join(inpath, testObj, "test")
-            testObjData = [[float(y) for y in x.replace("\n", "").split(" ") if y != ""] for x in open(os.path.join(testObjPath, random.choice(os.listdir(testObjPath))), "r").readlines()[1:]]
-            testData = np.ndarray(shape = (len(testObjData), 3), dtype = float)
-            for k in range(len(testObjData)):
-                for l in range(min(3,len(testObjData[i]))):
-                    testData[k][l] = testObjData[k][l]
-            testDataset.append((testData, i, key))
-
+            data = random.choice(testDict[random.choice(labelDict[key])])
+            testDataset.append((data, i, key))
+    
     random.shuffle(trainDataset)
     random.shuffle(testDataset)
     dataset = {
@@ -66,24 +65,24 @@ def generateFewshotData(way: int, shot: int, ind: int, labelDict: dict, inpath: 
     with open(os.path.join(fullOutpath, f"{ind}.pkl"), "wb") as f:
         pickle.dump(dataset, f)
 
-            
 
 def main():
     parser = argparse.ArgumentParser(description = "Create FewshotLearning Dataset for VoxML Attributes")
     parser.add_argument("attribute", help = "VoxML Attribute")
     parser.add_argument("-v", "--voxml", metavar = "", help = "Directory of VoxML files", required = True)
-    parser.add_argument("-d", "--dataset", metavar = "", help = "Directory of all 3D-Objects (see README.md for more information)", required = True)
+    parser.add_argument("--train", metavar = "", help = "train.dat file", required = True)
+    parser.add_argument("--test", metavar = "", help = "test.dat file", required = True)
     parser.add_argument("-o", "--output", metavar = "", help = "Outpath for Directories", required = True)
+    parser.add_argument("-w", "--way", type = int, metavar = "", help = "way", required = True)
+    parser.add_argument("-s", "--shot", type = int, metavar = "", help = "shot", required = True)
 
     args = parser.parse_args()
-    rawLabels = classifyObjects(args.voxml, args.attribute)
-
-    for way in [5,10]:
-        for shot in [10,20]:
-            for ind in range(10):
-                generateFewshotData(way, shot, ind, rawLabels, args.dataset, args.output)
-                print("Finished dataset => way: " + str(way) + ", shot: " + str(shot) + ", ind: " + str(ind))
-
+    labelList = ['airplane', 'bathtub', 'bed', 'bench', 'bookshelf', 'bottle', 'bowl', 'car', 'chair', 'cone', 'cup', 'curtain', 'desk', 'door', 'dresser', 'flower_pot', 'glass_box', 'guitar', 'keyboard', 'lamp', 'laptop', 'mantel', 'monitor', 'night_stand', 'person', 'piano', 'plant', 'radio', 'range_hood', 'sink', 'sofa', 'stairs', 'stool', 'table', 'tent', 'toilet', 'tv_stand', 'vase', 'wardrobe', 'xbox']
+    rawLabels = classifyObjects(args.voxml, args.attribute, labelList)
+    
+    for ind in range(10):
+        generateFewshotData(args.way, args.shot, ind, rawLabels, args.train, args.test, args.output)
+        print(f"Finished dataset {ind}!")
 
 if __name__ == "__main__":
     main()
--- VoxMLFewshotLearningDatasets/README.md
+++ VoxMLFewshotLearningDatasets/README.md
@@ -4,26 +4,17 @@ Create your own Fewshot-Learning-Dataset for VoxML-Attributes. Run
 ```
 python3 GenerateFewShotData.py <VoxML-Attribute>
     --voxml <VoxML-Directory>
-    --dataset <3D-Pointclouds>
+    --train <train.dat>
+    --test <test.dat>
     --output <outpath>
+    --way <way>
+    --shot <shot>
 ```
 
 The following rules apply:
 * VoxML-Attribute should include the full 'path' (for example Type/Concavity)
 * VoxML-Directory is a directory of VoxML files
-* 3D-Pointclouds follow the structure of ModelNet40:
-```
-│dataset/
-├──obj/
-│   ├──train/
-|       ├── ...           
-│   ├──test/
-|       ├── ...
-├──.......
+* train.dat and test.dat should be ModelNet40 datasets (see PointBERT)
 ```
 
 The generation is heavily based and made for [Point-BERT](https://github.com/lulutang0608/Point-BERT).
-
-## Credits
-
-All Pointcloud-Data was taken from the [ModelNet40-Dataset](https://modelnet.cs.princeton.edu/).
